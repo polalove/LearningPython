2c12044fcf91c6f4138eefa77d897c6e10e9e057
Ivan <example@gmail.com>
2024-05-22 00:53:37 +0300

    First day

---
+++ project.py
@@ -0,0 +1,57 @@
+# print(8 / 2 + 5 - (-3) / 2)
+
+# print(70 * (3 + 4) / (8 + 2))
+
+# print(0.39 * 0.22)
+
+# print(5 ** 2 - 3 * 7)
+
+# Символ экранирования  \
+# print("\"Khal Drogo's favorite word is \"athjahakar\"\"")
+
+# Энкранированая последовательность \n
+# print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
+
+# Конкатенация
+# print("Winter" + " came" + " for" + " the" + " House" + " of" + " Frey.")
+
+# print(chr(126)); print(chr(94)); print(chr(37))
+
+# motto = "What Is Dead May Never Die!"
+# print(motto)
+
+# name = 'Brienna'
+# name = 'anneirB'
+# print(name)
+
+# countBrothers = 2
+# print(countBrothers)
+
+# family = "Targaryen"
+# pet = "Dragon"
+#
+# print(family)
+# print(' and ')
+# print(pet)
+
+# eurosCount = 100
+# dollarsCount = eurosCount * 1.25
+# yuansCount = dollarsCount * 6.91
+#
+# print(dollarsCount)
+# print(yuansCount)
+
+
+eurosCount = 100
+dollarsCount = eurosCount * 1.25
+yuansCount = dollarsCount * 6.91
+valutesResult = f"{dollarsCount}\n{yuansCount}"
+
+print(valutesResult)
+
+
+
+
+
+
+
