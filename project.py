0744d967fe28ef268f5f335b7a14755a4578293d
Ivan <example@gmail.com>
2024-05-22 23:46:16 +0300

    Second day (30/72)

--- project.py
+++ project.py
@@ -15,6 +15,9 @@
 # Конкатенация
 # print("Winter" + " came" + " for" + " the" + " House" + " of" + " Frey.")
 
+# f"{}" - Интерполяция
+
+
 # print(chr(126)); print(chr(94)); print(chr(37))
 
 # motto = "What Is Dead May Never Die!"
@@ -29,29 +32,53 @@
 
 # family = "Targaryen"
 # pet = "Dragon"
-#
+
 # print(family)
 # print(' and ')
 # print(pet)
 
 # eurosCount = 100
 # dollarsCount = eurosCount * 1.25
-# yuansCount = dollarsCount * 6.91
-#
-# print(dollarsCount)
-# print(yuansCount)
+# yuan'sCount = dollarsCount * 6.91
+
+# print(f"{dollarsCount}\n{yuan'sCount}")
 
 
-eurosCount = 100
-dollarsCount = eurosCount * 1.25
-yuansCount = dollarsCount * 6.91
-valutesResult = f"{dollarsCount}\n{yuansCount}"
+# info = "We couldn't verify your mother's maiden name."
+# intro = "Here is important information about your account security."
+# first_name = 'Joffrey'
+# greeting = 'Hello'
 
-print(valutesResult)
-
-
-
+# print(greeting + ", " + first_name + "!")
+# print(intro + "\n" + info)
 
 
+# first_number = 20
+# second_number = -100
+# print(first_number * second_number)
+
+# castle_count = 6
+# room_count = 17
+# king = "Rooms in King Balon's Castle:"
+
+# print(king + f"\n{castle_count * room_count}")
+
+# DRAGONS_BORN_COUNT = 3
+# print(DRAGONS_BORN_COUNT)
+
+# stark = 'Arya'
+# print(f"Do you want to eat, {stark}?")
+
+# name = 'Na\nharis'
+# print(name[-1])
 
 
+# value = 'Hexlet'
+# print(value[2:5])
+
+# text = '''Lannister, Targaryen, Baratheon, Stark, Tyrell...
+# they're all just spokes on a wheel.
+# This one's on top, then that one's on top, and on and on it spins,
+# crushing those on the ground. '''
+
+# print(text)
